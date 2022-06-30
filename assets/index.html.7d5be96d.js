7a4ea3832d4c22b9f32e0cacc4f564f667c3952e
deancn <deancn@users.noreply.github.com>
2022-06-30 11:59:07 +0000

    deploy: 9999debda44f312e568f03a1ff6fb5ea244a603b

---
+++ assets/index.html.7d5be96d.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-de38e764","path":"/monitoring/","title":"Monitoring","lang":"en-US","frontmatter":{},"excerpt":"","headers":[{"level":2,"title":"Prometheus","slug":"prometheus","children":[]}],"git":{"updatedTime":1656590296000,"contributors":[{"name":"deancn","email":"dean@airdb.com","commits":1}]},"filePathRelative":"monitoring/README.md"}');export{e as data};
